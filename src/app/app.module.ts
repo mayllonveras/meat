d1e8a3c0ea6bc6431c949234195b7daf161b8c92
Mayllon Veras <mayllonveras@gmail.com>
2018-11-29 21:51:35 -0300

    Adicionados: Header, About e Restaurants

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component'
+import { ROUTES } from './app.routes';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    AboutComponent,
+    RestaurantsComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(ROUTES)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
